e6c33fb58319f0b3f8024cb4499581431e8ccca4
Сергей <trutnevss92@gmail.com>
2023-02-13 22:29:07 +1000

    task 1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import BasketPage from '../../pages/basket-page/basket-page';
+import CatalogPage from '../../pages/catalog-page/catalog-page';
+import ProductPage from '../../pages/product-page/product-page';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<CatalogPage />} />
+        <Route path='/product' element={<ProductPage />} />
+        <Route path='/basket' element={<BasketPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
