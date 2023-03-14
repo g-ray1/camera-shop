1582ac632040386c119f1f4a6896d93f5aa699b2
Сергей <trutnevss92@gmail.com>
2023-03-14 15:27:10 +1100

    фикс4

--- src/pages/product-page/product-page.tsx
+++ src/pages/product-page/product-page.tsx
@@ -10,7 +10,7 @@ import Slider from '../../components/slider/slider';
 import UpButton from '../../components/up-button/up-button';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { fetchCamera, fetchReviews, fetchSimilarCameras } from '../../store/api-actions';
-import { getCamera, getCameraIsLoading, getReviews, getReviewsIsLoading, getSimilarCameras, getSimilarCamerasIsLoading } from '../../store/data-slice/data-slice-selectors';
+import { getCamera, getCameraIsLoading, getReviews, getSimilarCameras, getSimilarCamerasIsLoading } from '../../store/data-slice/data-slice-selectors';
 import { getModalContent, getModalMode } from '../../store/utils-slice/utils-slice-selectors';
 import { UserParams } from '../../types/types';
 
